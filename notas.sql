4a7c4c347189ecb0381314c53cdd336c8d780cec
helozibetti <161315627+helozibetti@users.noreply.github.com>
2024-09-30 08:52:13 -0300

    Add files via upload
    fizemos o banco de dados

---
+++ notas.sql
@@ -0,0 +1,23 @@
+create database notas_heloize;
+use notas_heloize;
+
+create table conteudo (
+id_conteudo int not null primary key auto_increment,
+nome_conteudo varchar(255),
+conteudo_nota varchar(255),
+fk_categorias int not null,
+foreign key (fk_categorias) references categorias (id_categorias),
+fk_usuario int not null,
+foreign key (fk_usuario) references usuario (id_usuario)
+);
+
+create table usuario (
+id_usuario int not null primary key auto_increment,
+nome_usuario varchar(255)
+);
+
+create table categorias (
+id_categorias int not null primary key auto_increment,
+nome_categorias varchar(255)
+);
+
